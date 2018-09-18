56e7befdf272204255e7f37efb8f26c84ad921c7
Unknown <vladislav.kov@gmail.com>
2018-09-19 02:46:23 +0300

    added jslint and configs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-
-
-const Message = props => <div>{props.children}</div>
+const Message = props => <div>{props.children}</div>;
 
 class App extends Component {
   render() {
@@ -14,9 +12,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Message>qqq
-<Message>qqq2 </Message>
-
+        <Message>
+          qqq
+          <Message>qqq2 </Message>
         </Message>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
