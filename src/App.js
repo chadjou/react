b42158574c3e47106de3dc7bd17858ba949663fb
Unknown <vladislav.kov@gmail.com>
2018-09-19 15:56:50 +0300

    Conditionally Render A React Component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import { SayHello } from "./SayHello";
 
 const Message = props => <div>{props.children}</div>;
 
+class MessageIf extends Component {
+  render() {
+    const { message } = this.props;
+    return (
+      <div>{message ? <div> {message} </div> : <div> No message </div>}</div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,11 +22,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <MessageIf message="test" />
+        <MessageIf />
         <Message>
           qqq
           <Message>qqq2 </Message>
         </Message>
-       <SayHello firstName = "Jojo" lastName = "Freeman"/> 
+        <SayHello firstName="Jojo" lastName="Freeman" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
